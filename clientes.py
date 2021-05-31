ca02ab709522b541195c41c965f8273078663335
Kauã Alves <84326459+Kaua342@users.noreply.github.com>
2021-05-31 10:29:37 -0300

    Update clientes.py
    Fiz a segunda parte do arquivo, da linha 29 até 56

--- clientes.py
+++ clientes.py
@@ -26,154 +26,31 @@ class App(Funcs, Relatorios, Validadores):
         self.root.resizable(True, True)
         self.root.minsize(width = 700, height = 500)
         self.root.iconbitmap("icocli.ico")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   self.listaCli.heading("#1", text= "Código")
-   self.listaCli.heading("#2", text= "Nome")
-   self.listaCli.heading("#3", text= "Telefone")
-   self.listaCli.heading("#4", text= "Cidade")
-    
-   self.listaCli.column('#0', stretch=YES, minwidth=0,   width=0, anchor="center")
-   self.listaCli.column('#1', stretch=YES, minwidth=40,  width=50, anchor="center")
-   self.listaCli.column('#2', stretch=YES, minwidth=250, width=800, anchor="nw")
-   self.listaCli.column('#3', stretch=YES, minwidth=100, width=190, anchor="nw")
-   self.listaCli.column('#4', stretch=YES, minwidth=120, width=250, anchor="nw")
-   
-   self.barra_de_rolagem= scrollbar(self.frame_2, orient= "vertical")
-   self.listaCli.configure(yscroll= self.barra_de_rolagem.set)
-   self.barra_de_rolagem.place(relx=0.98, rely=0.052, relheight= 0.8962)
-   
-   self.listaCli.bind("<Double-1>", self.onDoubleClick)
- def Menus(self):
-   menubar= Menu(self.root)
-   self.root.config(menu=menubar)
-   filemenu= Menu(menubar, tearoff=0)
-   filemenu2= Menu(menubar, tearoff=0)
-  
-   def quit():
-      self.root.destroy()
-   
-   menubar.add_cascade(label= "Opções", menu= filemenu)
-   menubar.add_cascade(label="Relatórios", menu= filemenu2)
-   
-   filemenu.add_command(label="Limpa Cliente", command=self.limpa_tela)
-   filemenu.add_command(label="Sair", command=quit)
-    
-
+    def frames_da_tela(self):
+        self.frame_1=Frame(
+            self.root,
+             bd=4 #borda
+             bg="#363636", #cor de fundo
+             highlightbackground="#836FFF",
+             highlightthickness=1)
+        self.frame_1.place(
+            relx=0.01,
+            rely=0.02,
+            relwidth=0.98,
+            relheight=0.46)
+        self.frame_2=Frame(
+            self.root,
+            bd=4,
+            bg="#363636",
+            highlightbackground = "#836FFF",
+            highlightthickness=1)
+        self.frame_2.place(
+            relx=0.01,
+            rely=0.05,
+            relwidth=0.98,
+            relheight=0.46)
+    def widgets_frame1(self):
+        # Abas
+        self.abas=ttk.Notebook(self.frame_1)
+        self.aba1=Frame(self.abas)
+        self.aba2=Frame(self.abas)
