93fb7f35d78e08dfbaa8df12eb83853e9452c892
Gabriel Leopoldino da Silva <79914251+Leopoldino005@users.noreply.github.com>
2021-05-31 11:17:42 -0300

    Update clientes.py

--- clientes.py
+++ clientes.py
@@ -106,9 +106,15 @@ class App(Funcs, Relatorios, Validadores):
         self.bt_apagar.place(relex=0.6, reley=0.1, relwidh=0.1, relheight=0.1)
 
         #Criação da Label e Entrada do Código
-        self.Ib_codigo = Label(self.aba1, text="Código", bd = "#363636", fg = "#808080",font = ("trebouchet","10"))
+        self.Ib_codigo = Label(self.aba1, text="Código", bd = "#363636", fg = "#808080",font = ("Trebouchet","10"))
         self.Ib_codigo.place(relex = 0.008, reley = 0.02)
 
+        self.codigo_entry = Entry(self.aba1, validate='key', validatecommand = self.vcmd2)
+        self.codigo_entry.place(relx = 0.008, rely = 0.11, relwidth = 0.07, relheight = 0.08)
+        
+        # Label e Entrada do Nome
+        self.Ib_codigo = Label(self.aba1, text="nome", bd = "#363636", fg = "#808080",font = ("Trebouchet","10"))
+        
 
 
 
@@ -136,7 +142,53 @@ class App(Funcs, Relatorios, Validadores):
 
 
 
-
+        
+        
+        
+        
+        
+        
+        
+        
+   self.listaCli.heading("#1", text= "Código")
+   self.listaCli.heading("#2", text= "Nome")
+   self.listaCli.heading("#3", text= "Telefone")
+   self.listaCli.heading("#4", text= "Cidade")
+    
+   self.listaCli.column('#0', stretch=YES, minwidth=0,   width=0, anchor="center")
+   self.listaCli.column('#1', stretch=YES, minwidth=40,  width=50, anchor="center")
+   self.listaCli.column('#2', stretch=YES, minwidth=250, width=800, anchor="nw")
+   self.listaCli.column('#3', stretch=YES, minwidth=100, width=190, anchor="nw")
+   self.listaCli.column('#4', stretch=YES, minwidth=120, width=250, anchor="nw")
+   
+   self.barra_de_rolagem= scrollbar(self.frame_2, orient= "vertical")
+   self.listaCli.configure(yscroll= self.barra_de_rolagem.set)
+   self.barra_de_rolagem.place(relx=0.98, rely=0.052, relheight= 0.8962)
+   
+   self.listaCli.bind("<Double-1>", self.onDoubleClick)
+ def Menus(self):
+   menubar= Menu(self.root)
+   self.root.config(menu=menubar)
+   filemenu= Menu(menubar, tearoff=0)
+   filemenu2= Menu(menubar, tearoff=0)
+  
+   def quit():
+      self.root.destroy()
+   
+   menubar.add_cascade(label= "Opções", menu= filemenu)
+   menubar.add_cascade(label="Relatórios", menu= filemenu2)
+   
+   filemenu.add_command(label="Limpa Cliente", command=self.limpa_tela)
+   filemenu.add_command(label="Sair", command=quit)       
+        
+        
+        
+        
+        
+        
+        
+        
+       
   self.lb_nome.place(relx=0.008, rely=0.19
 
   self.nome_entry = Entry(self.aba1)
@@ -165,9 +217,9 @@ class App(Funcs, Relatorios, Validadores):
   self.popupMnu.place(relx=0.1, rely=0.1, relwidth=0.2, relheight=0.15)
   self.estado_civil = self.Tipvar.get()
  def lista_frame2(self):
-  self.listaCli = ttk.Treeview(self.frame_2, height = 3, column = ("col1", "col2", "col3", "col4"))
-  self.listaCli.place(relx = 0.01, rely = 0.05, relwidth = 0.98, relheigth = 0.9)
-  self.listaCli.heading("#0", text = " ")
+    self.listaCli = ttk.Treeview(self.frame_2, height = 3, column = ("col1", "col2", "col3", "col4"))
+    self.listaCli.place(relx = 0.01, rely = 0.05, relwidth = 0.98, relheigth = 0.9)
+    self.listaCli.heading("#0", text = " ")
                  
 
 
